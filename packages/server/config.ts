38fc003364f5c0f949c29430ffbee5c7744c4bc7
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-12-11 18:48:47 +0530

    config updated

--- packages/server/config.ts
+++ packages/server/config.ts
@@ -11,8 +11,19 @@ export const getAppConfig = () => {
 
 export const setupAppConfig = () => {
  const port = Number(process.env.PORT || DEFAULT_PORT);
+ const dbName = process.env.DB_NAME;
+ const mongoRootUsername = process.env.MONGO_INITDB_ROOT_USERNAME;
+ const mongoRootPassword = process.env.MONGO_INITDB_ROOT_PASSWORD;
+
+ if(!dbName || !mongoRootUsername || !mongoRootPassword) {
+    throw new Error("Missing environment variables, check .env file. DB_NAME, MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD are required.");
+ }
+
 
  config = {
   PORT: port,
+  DB_NAME: dbName,
+  MONGO_INITDB_ROOT_USERNAME: mongoRootUsername,
+  MONGO_INITDB_ROOT_PASSWORD: mongoRootPassword,
  };
 };
--- packages/server/src/types/config.ts
+++ packages/server/src/types/config.ts
@@ -1,3 +1,6 @@
 export type Config = {
  PORT: number;
+ MONGO_INITDB_ROOT_USERNAME: string;
+ MONGO_INITDB_ROOT_PASSWORD: string;
+ DB_NAME: string;
 };
