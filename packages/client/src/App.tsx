90680ec6fe2d22789155b56413670fd5fbe9e489
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-12-11 20:09:27 +0530

    vite client and tailwind setup done

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1 className="text-4xl font-bold text-gray-900 mb-8">Hello World</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
